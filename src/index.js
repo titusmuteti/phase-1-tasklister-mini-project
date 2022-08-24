61550cf06627178b5af55525b9dd21271fe5652a
Titus Mumo <titusmumo@gmail.com>
2022-08-24 20:56:22 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e)=>{
+    e.preventDefault()
+    createElement(e.target.querySelector('#new-task-description').value)
+  })
 });
+function createElement(TaskLister){
+  let li = document.createElement('li'); //creating li element
+
+  let btn = document.createElement('button') //creating a button
+
+  btn.textContent ='Remove' //adding button  text content 
+
+  li.textContent = `${TaskLister}  `; //adding li text content 
+
+  li.appendChild (btn) //append button as a child to li
+
+  btn.style.backgroundColor = 'yellow' //editing button background color
+
+  document.querySelector('#tasks').appendChild(li) // appending the li element
+
+  //adding eventlistener to the button
+  btn.addEventListener('click', (e)=>{
+    e.target.parentNode.remove();
+  })
+
+}
+
